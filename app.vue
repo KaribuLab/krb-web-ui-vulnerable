535ffb066cf9621b821c3a73dcdde74e947e1138
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-02-25 01:02:34 -0300

    feat: display runtime config secret key in app.vue

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Test</h1>
+    <h2>{{ mySecretKey }}</h2>
   </div>
 </template>
 
 <script lang="ts" setup>
+const mySecretKey = ref<string>('')
 const config = useRuntimeConfig()
 console.log('My secret is:', config.mySecretKey)
+mySecretKey.value = config.mySecretKey as string
 </script>
