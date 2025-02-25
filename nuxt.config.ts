9f060de07678299af34e4b6bf2be5f2b46cd6e9b
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-02-25 02:10:02 -0300

    fix: update bitbucket pipeline to set NODE_ENV to production and remove secret key from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
-console.log('process.env.MY_SECRET_API_KEY', process.env.MY_SECRET_API_KEY)
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: process.env.NODE_ENV === 'development' },
-  runtimeConfig: {
-    public: {
-      mySecretKey: process.env.MY_SECRET_API_KEY,
-    }
-  },
 })
