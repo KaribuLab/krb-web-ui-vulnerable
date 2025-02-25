89f3ed7e3f92a0add2ccb2c2f290c64f937c40c9
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-02-25 03:21:57 -0300

    fix: add NUXT_PUBLIC_BASE_URL to environment variables in workflows and app.vue

--- app.vue
+++ app.vue
@@ -2,12 +2,14 @@
   <div>
     <h1>Test</h1>
     <h2>My secret: {{ mySecretKey }}</h2>
+    <h2>My base url {{ baseUrl }}</h2>
   </div>
 </template>
 
 <script lang="ts" setup>
 const runtimeConfig = useRuntimeConfig()
-const mySecretKey = ref<string>('')
-console.log('My secret is:', runtimeConfig.public.NUXT_PUBLIC_MY_SECRET_API_KEY)
-mySecretKey.value = (runtimeConfig.public.NUXT_PUBLIC_MY_SECRET_API_KEY) as string
+const mySecretKey = ref<string>(runtimeConfig.public.NUXT_PUBLIC_MY_SECRET_API_KEY as string)
+const baseUrl = ref<string>(runtimeConfig.public.NUXT_PUBLIC_BASE_URL as string)
+console.log('My secret is:', mySecretKey.value)
+console.log('My base url is:', baseUrl.value)
 </script>
