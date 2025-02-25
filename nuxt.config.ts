820705832cfc8f55c5cbebd750a8fd5fbe26746f
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-02-25 01:54:20 -0300

    fix: update secret key handling to use public runtime config in app.vue and nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: process.env.NODE_ENV === 'development' },
   runtimeConfig: {
-    mySecretKey: process.env.MY_SECRET_API_KEY,
+    public: {
+      mySecretKey: process.env.MY_SECRET_API_KEY,
+    }
   },
 })
