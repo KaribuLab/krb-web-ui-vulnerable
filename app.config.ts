4ce25a9015829cb33d6f21cd0db91e8cb9c9e787
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-02-25 02:40:06 -0300

    fix: update appConfig to include mySecretKey and improve secret key logging in app.vue

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ const env = {
 }
 delete env.NETLIFY_AUTH_TOKEN
 delete env.NETLIFY_SITE_ID
-const appConfig = defineAppConfig(env)
+const appConfig = defineAppConfig({
+    ...env,
+    mySecretKey: env.MY_SECRET_API_KEY,
+})
 console.log('app.config.ts:', appConfig)
 export default useAppConfig
--- app.vue
+++ app.vue
@@ -9,6 +9,6 @@
 const mySecretKey = ref<string>('')
 const appConfig = useAppConfig()
 console.log('App config:', appConfig)
-console.log('My secret is:', appConfig.MY_SECRET_API_KEY)
-mySecretKey.value = appConfig.MY_SECRET_API_KEY as string
+console.log('My secret is:', appConfig.MY_SECRET_API_KEY ?? appConfig.mySecretKey)
+mySecretKey.value = (appConfig.MY_SECRET_API_KEY ?? appConfig.mySecretKey) as string
 </script>
