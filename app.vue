dec6364957a690460af490e547a6a22a5c45e953
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-02-25 03:35:02 -0300

    fix: refactor environment variable handling and update app configuration for secret key and base URL

--- app.vue
+++ app.vue
@@ -2,14 +2,14 @@
   <div>
     <h1>Test</h1>
     <h2>My secret: {{ mySecretKey }}</h2>
-    <h2>My base url {{ baseUrl }}</h2>
+    <h2>My base url: {{ baseUrl }}</h2>
   </div>
 </template>
 
 <script lang="ts" setup>
-const runtimeConfig = useRuntimeConfig()
-const mySecretKey = ref<string>(runtimeConfig.public.MY_SECRET_API_KEY as string)
-const baseUrl = ref<string>(runtimeConfig.public.PUBLIC_BASE_URL as string)
+const appConfig = useAppConfig()
+const mySecretKey = ref<string>(appConfig.MY_SECRET_API_KEY as string)
+const baseUrl = ref<string>(appConfig.BASE_URL as string)
 console.log('My secret is:', mySecretKey.value)
 console.log('My base url is:', baseUrl.value)
 </script>
