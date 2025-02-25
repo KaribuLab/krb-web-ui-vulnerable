e23fe6464fab0a31df1f0d404fe079c78cbbd0af
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-02-24 23:46:02 -0300

    fix: update environment variable handling for deployment scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: process.env.NODE_ENV === 'development' },
+  runtimeConfig: {
+    mySecretKey: process.env.MY_SECRET_KEY,
+  },
 })
