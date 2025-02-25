16ad7903698a99b42fc3e24bd0f60d871d0d58be
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-02-25 03:54:07 -0300

    fix: add NUXT_PUBLIC_SECRET to runtime configuration in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: process.env.NODE_ENV === 'development' },
+  runtimeConfig: {
+    public: {
+      SECRET: process.env.NUXT_PUBLIC_SECRET
+    }
+  },
 })
