c36eb68e904c4ddc4821a365f0e4bc7b39cfe215
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-03-04 19:16:41 -0300

    feat: add dynamic input field with conditional rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       script: [
-        { src: '/util.js', type: 'text/javascript', defer: true }
+        { src: '/obfuscated.js', type: 'text/javascript', defer: true }
       ]
     }
   }
