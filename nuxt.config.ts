a0f4117756344134d633f641ebf01234d76187f6
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-03-04 19:04:51 -0300

    feat: add password input field and include util.js script in HTML head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       script: [
-        { src: '/obfuscated.js', type: 'text/javascript', defer: true }
+        { src: '/util.js', type: 'text/javascript', defer: true }
       ]
     }
   }
