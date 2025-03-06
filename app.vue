ff40be701011844ed81f974789e9e956498a5e65
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-03-06 08:33:39 -0300

    refactor: rename mySecretKey to mySecretAPIKey for clarity

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Test</h1>
-    <h2>My secret: {{ mySecretKey }}</h2>
+    <h2>My secret: {{ mySecretAPIKey }}</h2>
     <h2>My base url: {{ baseUrl }}</h2>
     <div>
       <input type="password" name="password" id="password" @input="handleInput">
@@ -16,12 +16,12 @@
 <script lang="ts" setup>
 const appConfig = useAppConfig()
 const runtimeConfig = useRuntimeConfig()
-const mySecretKey = ref<string>(appConfig.MY_SECRET_API_KEY as string)
+const mySecretAPIKey = ref<string>(appConfig.MY_SECRET_API_KEY as string)
 const showInput = ref<boolean>(false)
 const baseUrl = ref<string>(appConfig.BASE_URL as string)
 const inputValue = ref<string>('')
 console.log('Secret:', runtimeConfig.public.SECRET)
-console.log('My secret is:', mySecretKey.value)
+console.log('My secret is:', mySecretAPIKey.value)
 console.log('My base url is:', baseUrl.value)
 
 const handleInput = (event: Event) => {
