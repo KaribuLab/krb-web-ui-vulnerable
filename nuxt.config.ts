6522bc053c96fa03788b4f97423068311bd3616c
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-03-06 09:30:36 -0300

    feat: add user creation and retrieval API methods with dynamic user ID display

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,8 @@ export default defineNuxtConfig({
   devtools: { enabled: process.env.NODE_ENV === 'development' },
   runtimeConfig: {
     public: {
-      MY_SECRET_API_KEY: process.env.NUXT_PUBLIC_SECRET
+      MY_SECRET_API_KEY: process.env.NUXT_PUBLIC_SECRET,
+      MY_READ_TOKEN: process.env.NUXT_PUBLIC_READ_TOKEN
     }
   },
   // Agregar util.js en el head del documento HTML con defer para ejecutarse cuando el DOM esté cargado
