dc2e8368e8f9fa98d8af995eb7dc37943c834b58
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-03-06 09:31:02 -0300

    feat: add Authorization header with runtime config secret API key

--- app.vue
+++ app.vue
@@ -45,6 +45,7 @@ const createUser = async () => {
 const getUser = async () => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId.value}`,{
     headers: {
+      'Authorization': `Bearer ${runtimeConfig.public.MY_SECRET_API_KEY}`,
       'X-Read-Token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
     }
   })
